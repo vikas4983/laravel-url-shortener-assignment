5690c6a94947b52472557bab03111c9e87ff8e79
Vikas4983 <vikaskumarofficial2023@gmail.com>
2025-12-16 00:22:02 +0530

    Set up superAdmin user login and disable public registration

--- routes/web.php
+++ routes/web.php
@@ -6,11 +6,9 @@ Route::get('/', function () {
     return view('welcome');
 });
 
-Route::middleware([
-    'auth:sanctum',
-    config('jetstream.auth_session'),
-    'verified',
-])->group(function () {
+
+
+Route::middleware(['auth'])->group(function () {
     Route::get('/dashboard', function () {
         return view('dashboard');
     })->name('dashboard');
