6c647a960dccd9d790336edadd4688483686424c
Vikas4983 <vikaskumarofficial2023@gmail.com>
2025-12-15 23:33:21 +0530

    add roles and SuperAdmin user

---
+++ database/seeders/RoleSeeder.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
+
+class RoleSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        // Role names
+        $roles = [
+            'SuperAdmin',
+            'Admin',
+            'Member',
+        ];
+        // Insert roles through loop 
+        foreach ($roles as  $role) {
+            DB::table('roles')->insert([
+                'name' => $role,
+                'created_at' => now(),
+                'updated_at' => now(),
+            ]);
+        }
+    }
+}
