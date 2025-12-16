5827b8d43941834a541f49eef658ed066cdc94ad
Vikas4983 <vikaskumarofficial2023@gmail.com>
2025-12-16 17:50:30 +0530

    Disable public registration and block register route

--- routes/web.php
+++ routes/web.php
@@ -6,12 +6,14 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
-
+Route::get('/register', function () {
+    abort(403, 'Registration is disabled.');
+})->name('register');
 
 
 Route::middleware(['auth'])->group(function () {
     Route::get('/dashboard', function () {
         return view('dashboard');
     })->name('dashboard');
-    Route::resource('companies',CompanyController::class);
+    Route::resource('companies', CompanyController::class);
 });
